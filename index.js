1329b3a024c71e1193061fb64927a5f85f23c41b
DaKingKong <41028904+DaKingKong@users.noreply.github.com>
2024-11-13 10:52:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 
 const app = express();
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+app.listen(5060, () => {
+    console.log('Server running on port 5060');
 });
 app.get('/is-alive', (req, res) => {
     res.send('Yes');
